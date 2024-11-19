300abc40f090bd7f50dac8e815894fd6cb078a52
Pratik-Tulkane14 <pratiktulkane12345@gmail.com>
2024-11-19 20:01:48 +0530

    Initial commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import  { useState } from 'react'
 import logo from "/BrandLogo.svg"
 import searchIcon from "/SearchIcon.svg"
  
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import headphone from "/headphone.png";
 
 const Hero = () => {
